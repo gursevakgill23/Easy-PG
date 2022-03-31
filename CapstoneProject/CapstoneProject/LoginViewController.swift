a606d41b1a6a317f4727a93b367342acfa7c4b79
bhavin <bhavin.pethani@ekzero.com>
2022-03-31 09:53:14 -0400

    LoginScreen With Functions

--- CapstoneProject/CapstoneProject/LoginViewController.swift
+++ CapstoneProject/CapstoneProject/LoginViewController.swift
@@ -2,28 +2,69 @@
 //  LoginViewController.swift
 //  CapstoneProject
 //
-//  Created by user202391 on 3/23/22.
+//  Created by Bhavin Pethani on 3/23/22.
 //
 
 import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var MobileNumber: UITextField!
+    
+    @IBOutlet weak var OtpTxt: UITextField!
+    
+    @IBOutlet weak var ErrorLbl: UILabel!
+    
+    var OTP = "123456"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // hide keyboard when user tap outside
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 
-        // Do any additional setup after loading the view.
+        view.addGestureRecognizer(tap)
+        
+        ErrorLbl.isHidden = true;
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // keyboard dismis function
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
-    */
-
+    
+    //login Button with Validation
+    @IBAction func LoginBtn(_ sender: Any) {
+        
+        if(MobileNumber.text?.count != 10){
+            
+            ErrorLbl.text = "Mobile Number Must be 10 Digits"
+            ErrorLbl.isHidden = false
+        }
+        else if(OtpTxt.text?.count != 6){
+            
+            ErrorLbl.text = "OTP Must be 6 Digits"
+            ErrorLbl.isHidden = false
+        }
+        else if(OTP != OtpTxt.text){
+            
+            ErrorLbl.text = "Wrong OTP"
+            ErrorLbl.isHidden = false
+        }
+        else{
+            let vc = storyboard?.instantiateViewController(withIdentifier: "userSelection") as! UserSelectionViewController
+            
+            vc.modalPresentationStyle = .fullScreen
+            present(vc,animated: true)
+            
+        }
+        
+        //hide label after 3 seconds 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.ErrorLbl.isHidden = true
+        }
+        
+    }
+    
+    
 }
