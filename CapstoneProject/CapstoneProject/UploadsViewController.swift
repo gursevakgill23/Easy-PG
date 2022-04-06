b6077d269589bbe69eddec4ae981d7b39014b633
gursevakgill2300 <69144067+gursevakgill2300@users.noreply.github.com>
2022-04-06 00:51:01 -0400

    Changes by Gursevak
    Changes in My Information Page

--- CapstoneProject/CapstoneProject/UploadsViewController.swift
+++ CapstoneProject/CapstoneProject/UploadsViewController.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class UploadsViewController: UIViewController,UIImagePickerControllerDelegate
-                             ,UINavigationControllerDelegate{
+                             ,UINavigationControllerDelegate,UITextFieldDelegate{
     @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var numberofBeds: UITextField!
     
+    @IBOutlet weak var pricetextField: UITextField!
+    @IBOutlet weak var uploadedImagesView: UIImageView!
+    @IBOutlet weak var uploadeditemsView: UITextView!
+    @IBOutlet weak var leaseTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addressTextField?.delegate = self
+        pricetextField?.delegate = self
+        leaseTextField?.delegate = self
+        numberofBeds?.delegate = self
+// Do any additional setup fter loading the view.
     }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addressTextField.resignFirstResponder()
+        leaseTextField.resignFirstResponder()
+        numberofBeds.resignFirstResponder()
+        pricetextField.resignFirstResponder()
+
+        return true
+    }
+
     func showImagePicker(sourceType:UIImagePickerController.SourceType)-> UIImagePickerController{
         let imgPicker = UIImagePickerController()
         imgPicker.sourceType = sourceType
@@ -30,5 +47,9 @@ class UploadsViewController: UIViewController,UIImagePickerControllerDelegate
         self.present(picker, animated: true)
     }
     
+    @IBAction func showItems(_ sender: Any) {
+        uploadeditemsView?.text = "Number of beds and baths:\(String(describing: numberofBeds.text))\nPrice of Appartment:\(String(describing: pricetextField.text))\nLease Time for sublet:\(String(describing: leaseTextField.text))\nAddress:\(String(describing: addressTextField.text))"
+    }
     
 }
+                                                                                                                                            
