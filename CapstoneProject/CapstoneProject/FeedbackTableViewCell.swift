f4ffe12ff76344714ced3443b80aaed4abc7983a
Teena Thomas <98230056+TeenaThoma@users.noreply.github.com>
2022-04-10 23:52:56 -0400

    teena changed
    teena changed

--- CapstoneProject/CapstoneProject/FeedbackTableViewCell.swift
+++ CapstoneProject/CapstoneProject/FeedbackTableViewCell.swift
@@ -28,7 +28,53 @@ class FeedbackTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    
+    let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 
+    
+
+    @IBOutlet weak var ErrorLb1: UILabel!
+    
+
+    @IBAction func btnSubmit(_ sender: Any) {
+        
+        
+    //login Button with Validation
+    
+        
+        if( txtFullname.text?.count == 0){
+            
+            ErrorLb1.text = "Full Name is Mandatory"
+            ErrorLb1.isHidden = false
+        }
+        else if(txtEmailid.text?.count == 0){
+            
+            ErrorLb1.text = "OTP Must be 6 Digits"
+            ErrorLb1.isHidden = false
+        }
+        else if(txtFeedback.text?.count == 0){
+            
+            ErrorLb1.text = "Wrong OTP"
+            ErrorLb1.isHidden = false
+        }
+        else{
+            let vc = storyboard?.instantiateViewController(withIdentifier: "userSelection") as! UserSelectionViewController
+            
+            vc.modalPresentationStyle = .fullScreen
+            present(vc,animated: true)
+            
+        }
+        
+        //hide label after 3 seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.ErrorLb1.isHidden = true
+        }
+        
+    }
+    
+    
+    
     @IBAction func btnClear(_ sender: Any) {
         
         txtFullname.text = ""
