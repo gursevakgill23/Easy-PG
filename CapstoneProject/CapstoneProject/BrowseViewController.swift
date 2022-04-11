120f5e2f32ebad970d914b4ae6a847b84ee7b773
gursevakgill2300 <69144067+gursevakgill2300@users.noreply.github.com>
2022-04-11 11:47:54 -0400

    changes
    changes

--- CapstoneProject/CapstoneProject/BrowseViewController.swift
+++ CapstoneProject/CapstoneProject/BrowseViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class BrowseViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +15,5 @@ class BrowseViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
